6fe6e2e6b3bd75b93d09c32b27f932a20c7b953d
Nijanthan <nijanth.niji@gmail.com>
2019-09-29 20:00:15 +0530

    Added x-auth-token while calling backend services

--- src/services/httpServices.jsx
+++ src/services/httpServices.jsx
@@ -4,7 +4,6 @@ import { getTokenFromCookie } from "../util/authentication.jsx";
 import { AUTH_TOKEN } from "./../util/authentication";
 import { toast } from "react-toastify";
 
-axios.defaults.headers.post[AUTH_TOKEN] = getTokenFromCookie();
 axios.interceptors.response.use(null, error => {
   console.log(error);
   if (!error.response) {
@@ -16,6 +15,8 @@ axios.interceptors.response.use(null, error => {
   if (data) toast.error(data);
   return { status, data: [] };
 });
+
+axios.defaults.headers.post[AUTH_TOKEN] = getTokenFromCookie();
 
 export const httpPost = async (requestMapping, requestBody) => {
   return await axios.post(apiEndPoint + requestMapping, requestBody);
